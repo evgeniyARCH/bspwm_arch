4861bb32c580850156895e718b254205bda1bd01
evgeniyARCH <123307679+evgeniyARCH@users.noreply.github.com>
2023-01-27 20:33:34 +0500

    Add files via upload

---
+++ bspvm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp /home/$USER/bspvm1/bspwm /home/$USER/.config/
+cp /home/$USER/bspvm1/jgmenu /home/$USER/.config/
+cp /home/$USER/bspvm1/neofetch /home/$USER/.config/
+echo "complete"
